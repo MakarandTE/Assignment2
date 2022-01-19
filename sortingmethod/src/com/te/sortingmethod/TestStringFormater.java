927ce5e35304cdd3d3be2492b4bc9c0cc7c2f091
makarand burud <makarand.burud13@gmail.com>
2022-01-19 19:01:23 +0530

    updated

---
+++ sortingmethod/src/com/te/sortingmethod/TestStringFormater.java
@@ -0,0 +1,11 @@
+package com.te.sortingmethod;
+
+public class TestStringFormater {
+	public static void main(String[] args) {
+		
+	
+	
+	System.out.println(StringFormatter.reverseString("my name is khan"));  
+    System.out.println(StringFormatter.reverseString("I am sonoo jaiswal")); 
+}
+}
